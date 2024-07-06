2da1f99bb614622eb19b4f81d41fda53592e3951
megisekhnishvili <megi.sekhniashvili.1@btu.edu.ge>
2024-07-06 23:45:39 +0400

    fifteenth commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useEffect } from 'react';
+import { useLocalStorage, useWindowSize } from './hooks';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useLocalStorage('theme', 'light');
+  const size = useWindowSize();
+
+  const isMobile = size.width <= 768;
+
+  useEffect(() => {
+    if (isMobile) {
+      setTheme('light');
+    }
+  }, [isMobile, setTheme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`app ${theme}`}>
+      <header className="app-header">
+        <h1>{`Current theme: ${theme}`}</h1>
+        <button onClick={toggleTheme} disabled={isMobile}>
+          Toggle Theme
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks.js
@@ -0,0 +1,51 @@
+// src/hooks.js
+
+import { useState, useEffect } from 'react';
+
+
+export function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
